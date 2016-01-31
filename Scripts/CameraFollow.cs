cf0b26e9bdd889b1542bd231955e9851cf775f0a
mikebjoyce <mikebjoyce@gmail.com>
2016-01-31 12:52:20 -0500

    lisnvksdjvnsldkfnvsldfk

--- Scripts/CameraFollow.cs
+++ Scripts/CameraFollow.cs
@@ -4,22 +4,10 @@ using System.Collections;
 public class CameraFollow : MonoBehaviour {
 
 	public Transform linkToWitch;
-	public Transform linkToCamera;
-	private float posx;
-	private float posy;
 
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
 	void Update () {
-		//set camera x, y to that of witch
-		//linkToCamera.position.x = posx;
-		//linkToWitch.position.x = posx;
-		//linkToCamera.position.y = posy;
-		//linkToWitch.position.y = posy;
+		transform.position = linkToWitch.position + new Vector3 (0, 0, -10);
 	}
 }
