ee62e88024bc3a780ba4dcc1101cbcfec297fda9
mikebjoyce <mikebjoyce@gmail.com>
2016-01-31 01:00:32 -0500

    PICTURES

--- Scripts/EventElements.cs
+++ Scripts/EventElements.cs
@@ -9,6 +9,20 @@ public class EventElements{
 	//public string[] elementQualifiers = {"ether", "air", "fire", "water", "light", "dark", "critter/insect", "beast"};
 	//each series of two strings indicate the element and its opposite.
 
+	EventComponents linkToCalc;
+	int[] absMax = new int[2];
+	int[] absMaxIndexes = new int[2];
+	public EventElements(float[] input){
+		linkToCalc = new EventComponents (input);
+		absMaxIndexes = linkToCalc.getIndexOfTwoAbsMax ();
+		absMax = linkToCalc.getIndexOfTwoAbsMax ();
+	}
+
+	public string[] getDescribers(){
+		string[] tempString = mainDescriptors ();
+		tempString += secondaryAdjectives ();
+		return tempString;
+	}
 
 	//note as power decreases from index 0 to 3. Need 3 adjectives for each.
 	public string[] etherAdjectives = {"volotile", "strangly powerful", "etherreal"};
@@ -20,6 +34,17 @@ public class EventElements{
 	public string[] critterAdjectives = { "teaming", "crawling", "pesky"};
 	public string[] beastAdjectives = { "rampaging", "stampeding", "beastial" };
 
+	public string[][] secondaryAdjectives = {
+		etherAdjectives,
+		airAdjectives,
+		fireAdjectives,
+		waterAdjectives,
+		lightAdjectives,
+		darkAdjectives,
+		critterAdjectives,
+		beastAdjectives
+	};
+
 	public string[] etherMainDescriber = { "Thick black fog", "Lake of smoke" };
 	public string[] airMainDescriber = { "Hurricane", "Squall" };
 	public string[] fireMainDescriber = { "Fire Storm", "Wild fire"};
@@ -28,6 +53,46 @@ public class EventElements{
 	public string[] darkMainDescriber = { "Diabolical presence", "Unholy aura" };
 	public string[] critterMainDescriber = { "Plague of Locusts", "Swarm of Rats" };
 	public string[] beastMainDescriber = {"Pack of hungry Wolves", "Roaming herd of Buffalo"};
+
+	public string[][] mainDescriptors = {
+		etherMainDescriber,
+		airMainDescriber,
+		fireMainDescriber,
+		waterMainDescriber,
+		lightMainDescriber,
+		darkMainDescriber,
+		critterMainDescriber,
+		beastMainDescriber
+	};
+
+	public string[] retMainDescriptors(Globals.energySubTypes type, float power){
+		int intType = (int)type; 
+		power = Mathf.Abs (power);
+		if (power > 20) {
+			power = 0;
+		}else{
+			power = 1;
+		}
+		return mainDescriptors[intType][power];
+	}
+
+	public string[] retSecondaryDescriptors(Globals.energySubTypes type, float power){
+		int intType = (int)type; 
+		power = Mathf.Abs (power);
+		if (power > 20) {
+			power = 0;
+		}else if(power > 10){
+			power = 1;
+		}else{
+			power = 2;
+		}
+		return secondaryAdjectives[intType][power];
+	}
+
+
+	//(1) when player summon a storm(thing) have it described
+	//(2) have npc describe something to player in witch hut scene
+	//(3) 
 
 	//public EventComponents workingEvent = new EventComponents ();
 }
